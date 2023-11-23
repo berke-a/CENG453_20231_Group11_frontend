047507c7842cc9cab47b4af53a8bcac4a9539d8c
berke <berke.ahlatci@gmail.com>
2023-11-23 14:31:36 +0300

    rename application

--- src/main/java/com/example/ceng453_20231_group11_frontend/CatanApplication.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/CatanApplication.java
@@ -1,5 +1,6 @@
 package com.example.ceng453_20231_group11_frontend;
 
+import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,12 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CatanApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CatanApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle(GeneralConstants.WINDOW_TITLE);
         stage.setScene(scene);
         stage.show();
     }
