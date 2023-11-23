c8e366459d7368bf2467e19a4099a2dee2296e8d
berke <berke.ahlatci@gmail.com>
2023-11-23 15:33:15 +0300

    create constants

---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/constants/GeneralConstants.java
@@ -0,0 +1,13 @@
+package com.example.ceng453_20231_group11_frontend.constants;
+
+public class GeneralConstants {
+
+    // App Properties
+    public static final String WINDOW_TITLE = "HexaLands";
+    public static final int WINDOW_WIDTH = 900;
+    public static final int WINDOW_HEIGHT = 800;
+
+
+    // FXML Locations
+    public static final String REGISTER_PAGE = "fxml/register.fxml";
+}
