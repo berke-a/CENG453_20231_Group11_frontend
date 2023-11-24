8d4bc10bcea848c217afe12d440c6bd86ce43c57
berke <berke.ahlatci@gmail.com>
2023-11-25 01:17:47 +0300

    implement action function

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/LoginController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/LoginController.java
@@ -1,4 +1,11 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
+    @FXML
+    public void onClickLoginButton(ActionEvent event) {
+        System.out.println("Login button clicked!");
+    }
 }
