45aaf17b66f5f8508dd1087cc73e2ff3aa7fb6ff
berke <berke.ahlatci@gmail.com>
2023-11-24 19:10:32 +0300

    implement onClickRouteRegister

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/HomeController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/HomeController.java
@@ -1,12 +1,23 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
+import com.example.ceng453_20231_group11_frontend.CatanApplication;
+import com.example.ceng453_20231_group11_frontend.Utils;
+import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 
 public class HomeController {
 
     @FXML
-    protected void onClickRouteRegister() {
-        System.out.println("Register button clicked!");
+    public void onClickRouteRegister(ActionEvent event) {
+        try {
+            Parent registerPage = CatanApplication.loadFXML(GeneralConstants.REGISTER_PAGE);
+            Utils.routeToPage(event, registerPage);
+            System.out.println("Succesfully routed to register page!");
+        } catch (Exception e) {
+            System.out.println("An error occured: " + e.getMessage());
+        }
     }
 
     @FXML
