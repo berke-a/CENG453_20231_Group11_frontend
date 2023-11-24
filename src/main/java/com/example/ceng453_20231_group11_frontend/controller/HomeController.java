df561cd2c06a0fbb62e3b0a1c940192f89655e21
berke <berke.ahlatci@gmail.com>
2023-11-24 18:16:36 +0300

    implement controller name

---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.ceng453_20231_group11_frontend.controller;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    protected void onClickRouteRegister() {
+        System.out.println("Register button clicked!");
+    }
+
+    @FXML
+    protected void onClickRouteLogin() {
+        System.out.println("Login button clicked!");
+    }
+
+    @FXML
+    protected void onClickRouteLeaderboard() {
+        System.out.println("Leaderboard button clicked!");
+    }
+
+}
