9aa70ac3f2182e62cb1d1d507866de548fac069c
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-04 21:33:50 +0300

    Update routeToPage in Utils

--- src/main/java/com/example/ceng453_20231_group11_frontend/Utils.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/Utils.java
@@ -2,18 +2,18 @@ package com.example.ceng453_20231_group11_frontend;
 
 import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Utils {
-    public static void routeToPage(ActionEvent event, Parent newPage) throws IOException {
+    public static void routeToPage(ActionEvent event, Parent newPage) {
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent currentRoot = currentStage.getScene().getRoot();
+        NavigationHistoryManager.push(currentRoot);
         Scene newScene = new Scene(newPage, GeneralConstants.WINDOW_WIDTH, GeneralConstants.WINDOW_HEIGHT);
 
         currentStage.setScene(newScene);
