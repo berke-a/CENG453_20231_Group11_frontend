af825ce45c56e8593e670cfeac6935fdb13865a3
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-04 21:35:04 +0300

    Add back button logic to Forgot Password Page

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/ForgotPasswordController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/ForgotPasswordController.java
@@ -1,12 +1,16 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
 import com.example.ceng453_20231_group11_frontend.CatanApplication;
+import com.example.ceng453_20231_group11_frontend.NavigationHistoryManager;
 import com.example.ceng453_20231_group11_frontend.Utils;
 import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ForgotPasswordController {
     @FXML
@@ -21,6 +25,22 @@ public class ForgotPasswordController {
             System.out.println("Successfully routed to reset password page!");
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    public void onClickBackButton(ActionEvent event) {
+        Parent previousPage = NavigationHistoryManager.pop();
+        if (previousPage != null) {
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene existingScene = previousPage.getScene();
+
+            if (existingScene == null) {
+                existingScene = new Scene(previousPage);
+            }
+
+            currentStage.setScene(existingScene);
+            currentStage.show();
         }
     }
 }
