965afe331792e4651ee22d23aea1026c65635298
oguzhangumus <90193580+oguzhangumus@users.noreply.github.com>
2023-12-04 16:44:49 +0300

    Oguzhan/leaderboard page (#6)
    * add leaderboard fxml
    
    * connect leaderboard with db
    
    * add dbService

---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/LeaderboardResponse.java
@@ -0,0 +1,13 @@
+package com.example.ceng453_20231_group11_frontend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LeaderboardResponse {
+    private Object data;
+    private String message;
+    private String result;
+
+}
