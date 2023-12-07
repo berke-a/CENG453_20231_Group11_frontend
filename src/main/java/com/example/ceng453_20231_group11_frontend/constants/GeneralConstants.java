0c4da1790cbf0f366d3953228fc8cefa31300081
berke <berke.ahlatci@gmail.com>
2023-12-07 20:20:17 +0300

    add path

--- src/main/java/com/example/ceng453_20231_group11_frontend/constants/GeneralConstants.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/constants/GeneralConstants.java
@@ -15,4 +15,5 @@ public class GeneralConstants {
     public static final String LEADERBOARD_PAGE = "fxml/leaderboard.fxml";
     public static final String FORGOT_PASSWORD_PAGE = "fxml/forgot-password.fxml";
     public static final String RESET_PASSWORD_PAGE = "fxml/reset-password.fxml";
+    public static final String BOARD_PAGE = "fxml/board.fxml";
 }
