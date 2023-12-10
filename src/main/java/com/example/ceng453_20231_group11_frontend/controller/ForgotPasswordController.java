fced57d4bdf0c5ae06721ed45fb0c8ec06d0f324
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-10 18:35:47 +0300

    Add backend connection to Forgot Password

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/ForgotPasswordController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/ForgotPasswordController.java
@@ -4,13 +4,16 @@ import com.example.ceng453_20231_group11_frontend.CatanApplication;
 import com.example.ceng453_20231_group11_frontend.NavigationHistoryManager;
 import com.example.ceng453_20231_group11_frontend.Utils;
 import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
+import com.example.ceng453_20231_group11_frontend.service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 public class ForgotPasswordController {
     @FXML
@@ -18,14 +21,30 @@ public class ForgotPasswordController {
 
     @FXML
     private void onClickSubmitButton(ActionEvent event) {
-        try {
-            String email = emailField.getText();
-            Parent resetPasswordPage = CatanApplication.loadFXML(GeneralConstants.RESET_PASSWORD_PAGE);
-            Utils.routeToPage(event, resetPasswordPage);
-            System.out.println("Successfully routed to reset password page!");
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
+        String email = emailField.getText();
+        Pair<Integer, String> response = UserService.requestPasswordReset(email);
+
+        if (response.getKey() == 200) {
+            showAlert(Alert.AlertType.INFORMATION, "Success", "Password reset email sent successfully.");
+            try {
+                Parent resetPasswordPage = CatanApplication.loadFXML(GeneralConstants.RESET_PASSWORD_PAGE);
+                Utils.routeToPage(event, resetPasswordPage);
+                System.out.println("Successfully routed to reset password page!");
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
+                showAlert(Alert.AlertType.ERROR, "Error", "Error processing the request.");
+            }
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Error", "Failed to send password reset email. " + response.getValue());
         }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait(); // This will block until the user closes the alert
     }
 
     @FXML
