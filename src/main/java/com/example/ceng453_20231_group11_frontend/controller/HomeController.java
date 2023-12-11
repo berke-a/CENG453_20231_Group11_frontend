b8752876860d06a9f4b6cbb86037dabf5a8376d7
berke <berke.ahlatci@gmail.com>
2023-12-11 21:55:40 +0300

    update button visibility according to logged user

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/HomeController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/HomeController.java
@@ -1,17 +1,36 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
-import com.example.ceng453_20231_group11_frontend.CatanApplication;
 import com.example.ceng453_20231_group11_frontend.Utils;
 import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class HomeController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
 
     @FXML
     private Button playButton;
+
+    @FXML
+    private Button routeRegisterButton;
+
+    @FXML
+    private Button routeLoginButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resource) {
+        Boolean isLoggedIn = Utils.isLoggedIn();
+        if (isLoggedIn) {
+            routeRegisterButton.setVisible(false);
+            routeLoginButton.setVisible(false);
+        } else {
+            playButton.setVisible(false);
+        }
+    }
 
     @FXML
     protected void onClickPlay(ActionEvent event) {
