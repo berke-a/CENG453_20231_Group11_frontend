0669d3b36fdb83c0992f44fe8571440cbf1b170b
berke <berke.ahlatci@gmail.com>
2023-12-12 15:01:46 +0300

    change asset extension and add locations to constants

--- src/main/java/com/example/ceng453_20231_group11_frontend/constants/GeneralConstants.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/constants/GeneralConstants.java
@@ -17,4 +17,19 @@ public class GeneralConstants {
     public static final String FORGOT_PASSWORD_PAGE = "fxml/forgot-password.fxml";
     public static final String RESET_PASSWORD_PAGE = "fxml/reset-password.fxml";
     public static final String BOARD_PAGE = "fxml/board.fxml";
+
+    // Image Locations
+    public static final String TILE_BRICK = "/com/example/ceng453_20231_group11_frontend/images/tile_brick.png";
+    public static final String TILE_DESERT = "/com/example/ceng453_20231_group11_frontend/images/tile_desert.png";
+    public static final String TILE_LUMBER = "/com/example/ceng453_20231_group11_frontend/images/tile_lumber.png";
+    public static final String TILE_ORE = "/com/example/ceng453_20231_group11_frontend/images/tile_ore.png";
+    public static final String TILE_WOOL = "/com/example/ceng453_20231_group11_frontend/images/tile_wool.png";
+    public static final String TILE_GRAIN = "/com/example/ceng453_20231_group11_frontend/images/tile_grain.png";
+    public static final String TILE_WATER = "/com/example/ceng453_20231_group11_frontend/images/tile_water.png";
+    public static final String RESOURCE_BRICK = "/com/example/ceng453_20231_group11_frontend/images/resource_brick.png";
+    public static final String RESOURCE_LUMBER = "/com/example/ceng453_20231_group11_frontend/images/resource_lumber.png";
+    public static final String RESOURCE_ORE = "/com/example/ceng453_20231_group11_frontend/images/resource_ore.png";
+    public static final String RESOURCE_WOOL = "/com/example/ceng453_20231_group11_frontend/images/resource_wool.png";
+    public static final String RESOURCE_GRAIN = "/com/example/ceng453_20231_group11_frontend/images/resource_grain.png";
+
 }
