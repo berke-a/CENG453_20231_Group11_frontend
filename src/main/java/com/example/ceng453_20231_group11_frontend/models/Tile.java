22129019a4f91082582bde7170dd2021d295f8f7
berke <berke.ahlatci@gmail.com>
2023-12-12 15:17:01 +0300

    create tile class

---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Tile.java
@@ -0,0 +1,15 @@
+package com.example.ceng453_20231_group11_frontend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tile {
+    private ResourceType resourceType;
+    private int numberToken;
+}
