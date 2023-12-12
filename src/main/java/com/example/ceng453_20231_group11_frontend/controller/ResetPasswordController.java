b75db1a574ca26a37049991ca9e3b9b94c20ee16
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-13 01:36:28 +0300

    Add loader to Reset Password page

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/ResetPasswordController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/ResetPasswordController.java
@@ -5,6 +5,7 @@ import com.example.ceng453_20231_group11_frontend.Utils;
 import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import com.example.ceng453_20231_group11_frontend.service.UserService;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.Pair;
@@ -36,6 +38,9 @@ public class ResetPasswordController implements Initializable {
     @FXML
     private Pane pane;
 
+    @FXML
+    private ProgressIndicator loader;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Platform.runLater(() -> pane.requestFocus());
@@ -52,13 +57,41 @@ public class ResetPasswordController implements Initializable {
             return;
         }
 
-        Pair<Integer, String> response = UserService.changePassword(token, newPassword);
-        if (response.getKey() == 200) {
-            Utils.showAlert(Alert.AlertType.INFORMATION, "Success", "Password successfully changed.");
-            Utils.routeToPage(event, GeneralConstants.HOME_PAGE);
-        } else {
-            Utils.showAlert(Alert.AlertType.ERROR, "Error", "Failed to change password. " + response.getValue());
-        }
+        // Show the loader
+        loader.setVisible(true);
+
+        // Create the background task for password reset
+        Task<Pair<Integer, String>> passwordResetTask = new Task<Pair<Integer, String>>() {
+            @Override
+            protected Pair<Integer, String> call() {
+                return UserService.changePassword(token, newPassword);
+            }
+        };
+
+        // Set the on succeeded event handler
+        passwordResetTask.setOnSucceeded(e -> {
+            // Hide the loader
+            loader.setVisible(false);
+
+            Pair<Integer, String> response = passwordResetTask.getValue();
+
+            if (response.getKey() == 200) {
+                Utils.showAlert(Alert.AlertType.INFORMATION, "Success", "Password successfully changed.");
+                Utils.routeToPage(event, GeneralConstants.HOME_PAGE);
+            } else {
+                Utils.showAlert(Alert.AlertType.ERROR, "Error", response.getValue());
+            }
+        });
+
+        // Set the on failed event handler
+        passwordResetTask.setOnFailed(e -> {
+            // Hide the loader and show the error message
+            loader.setVisible(false);
+            Utils.showAlert(Alert.AlertType.ERROR, "Error", "An error occurred during password reset.");
+        });
+
+        // Start the task on a background thread
+        new Thread(passwordResetTask).start();
     }
 
     @FXML
