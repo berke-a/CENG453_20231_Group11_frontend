72a19d989dff55c5c2d4c7d63456286438775656
berke <berke.ahlatci@gmail.com>
2023-12-12 15:20:59 +0300

    start the implementation of intialize

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/BoardController.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/BoardController.java
@@ -1,7 +1,13 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
-public class BoardController {
+import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 
+import java.net.URL;
+import java.util.*;
+
+public class BoardController extends BoardControllerAbstract {
     // Hexagon
     // Player
     // Dice
@@ -13,4 +19,22 @@ public class BoardController {
 
     // Roll Dice Action Listener
     // Buy Game Piece Action Listener
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List keys = new ArrayList(this.tileMap.keySet());
+        Collections.shuffle(keys);
+        for (Object o : keys) {
+            this.tileMap.get(o);
+        }
+
+        try {
+            Image img = new Image(Objects.requireNonNull(getClass().getResourceAsStream(GeneralConstants.TILE_DESERT)));
+            System.out.println(img);
+            System.out.println(new ImagePattern(img));
+            this.tile1.setFill(new ImagePattern(img));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
