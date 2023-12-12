89d0d2f8e2257725e00b6bb7e4240830fb4b55d5
berke <berke.ahlatci@gmail.com>
2023-12-13 00:41:41 +0300

    update board abstract

--- src/main/java/com/example/ceng453_20231_group11_frontend/controller/BoardControllerAbstract.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/controller/BoardControllerAbstract.java
@@ -1,12 +1,15 @@
 package com.example.ceng453_20231_group11_frontend.controller;
 
+import com.example.ceng453_20231_group11_frontend.constants.GeneralConstants;
 import com.example.ceng453_20231_group11_frontend.models.Tile;
 import com.example.ceng453_20231_group11_frontend.models.TileType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
 
-import java.util.HashMap;
+import java.util.*;
 
 abstract class BoardControllerAbstract implements Initializable {
     @FXML
@@ -15,14 +18,22 @@ abstract class BoardControllerAbstract implements Initializable {
 
     protected HashMap<Polygon, Tile> tileMap = new HashMap<Polygon, Tile>();
 
+    protected HashMap<TileType, String> tileTypeToImage = new HashMap<TileType, String>() {{
+        put(TileType.DESERT, GeneralConstants.TILE_DESERT);
+        put(TileType.LUMBER, GeneralConstants.TILE_LUMBER);
+        put(TileType.BRICK, GeneralConstants.TILE_BRICK);
+        put(TileType.GRAIN, GeneralConstants.TILE_GRAIN);
+        put(TileType.WOOL, GeneralConstants.TILE_WOOL);
+        put(TileType.ORE, GeneralConstants.TILE_ORE);
+    }};
+
     protected TileType[] tileTypes = {TileType.DESERT, TileType.LUMBER, TileType.LUMBER, TileType.LUMBER, TileType.LUMBER, TileType.BRICK, TileType.BRICK, TileType.BRICK, TileType.GRAIN, TileType.GRAIN, TileType.GRAIN, TileType.GRAIN,
             TileType.WOOL, TileType.WOOL, TileType.WOOL, TileType.ORE, TileType.ORE, TileType.ORE};
 
     protected int[] tileNumberTokens = {2, 3, 3, 4, 4, 5, 5, 6, 6, 8,
             8, 9, 9, 10, 10, 11, 11, 12};
 
-
-    BoardControllerAbstract() {
+    protected void initializeTiles() {
         tileMap.put(tile1, new Tile());
         tileMap.put(tile2, new Tile());
         tileMap.put(tile3, new Tile());
@@ -42,5 +53,21 @@ abstract class BoardControllerAbstract implements Initializable {
         tileMap.put(tile17, new Tile());
         tileMap.put(tile18, new Tile());
         tileMap.put(tile19, new Tile());
+
+        this.populateRandomTiles();
+    }
+
+    protected void populateRandomTiles() {
+        List<Polygon> keys = new ArrayList<>(this.tileMap.keySet());
+        Collections.shuffle(keys);
+        this.tileMap.get(keys.get(0)).setTileType(TileType.DESERT);
+
+        for (int i = 1; i < keys.size(); i++) {
+            this.tileMap.get(keys.get(i)).setTileType(this.tileTypes[i]);
+            this.tileMap.get(keys.get(i)).setNumberToken(this.tileNumberTokens[i - 1]);
+            Image img = new Image(Objects.requireNonNull(getClass().getResourceAsStream(this.tileTypeToImage.get(this.tileTypes[i]))));
+            keys.get(i).setFill(new ImagePattern(img));
+        }
+
     }
 }
