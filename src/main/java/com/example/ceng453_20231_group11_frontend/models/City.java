a9dc0d348fd5fde4462a87c74675bb5e43dec789
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-13 20:01:05 +0300

    Utku/Buildings (#29)
    * Create city model
    
    * Create Road model
    
    * Create Settlement model
    
    * Remove comments
    
    * Add victory point to city and settlement

---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/City.java
@@ -0,0 +1,14 @@
+package com.example.ceng453_20231_group11_frontend.models;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class City extends Group {
+    private final Integer victoryPoint = 2;
+    public City() {
+        Rectangle rectangle = new Rectangle(20, 20);
+        rectangle.setFill(Color.RED);
+        this.getChildren().add(rectangle);
+    }
+}
---
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Settlement.java
@@ -0,0 +1,20 @@
+package com.example.ceng453_20231_group11_frontend.models;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+public class Settlement extends Group {
+    private final Integer victoryPoint = 1;
+    public Settlement() {
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(new Double[]{
+                0.0, 0.0,
+                10.0, 10.0,
+                0.0, 20.0,
+                -10.0, 10.0
+        });
+        polygon.setFill(Color.BLACK);
+        this.getChildren().add(polygon);
+    }
+}
