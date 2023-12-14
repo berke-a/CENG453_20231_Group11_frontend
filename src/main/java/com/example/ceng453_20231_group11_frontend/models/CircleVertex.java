97326fbcd9210c3aa65bc55435b5f6033bfe2e20
berke <berke.ahlatci@gmail.com>
2023-12-14 19:45:18 +0300

    update CircleVertex

--- src/main/java/com/example/ceng453_20231_group11_frontend/models/CircleVertex.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/CircleVertex.java
@@ -1,5 +1,7 @@
 package com.example.ceng453_20231_group11_frontend.models;
 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +15,8 @@ import java.util.List;
 public class CircleVertex {
     private boolean hasSettlement;
     private boolean hasCity;
-    private List<CircleVertex> adjacentCircles;
+    private List<Circle> adjacentCircles;
+    private List<Polygon> adjacentTiles;
 
     public CircleVertex() {
         this.hasSettlement = false;
@@ -21,9 +24,10 @@ public class CircleVertex {
         this.adjacentCircles = new ArrayList<>();
     }
 
-    public CircleVertex(List<CircleVertex> adjacentCircles) {
+    public CircleVertex(List<Circle> adjacentCircles, List<Polygon> adjacentTiles) {
         this.hasSettlement = false;
         this.hasCity = false;
         this.adjacentCircles = adjacentCircles;
+        this.adjacentTiles = adjacentTiles;
     }
 }
