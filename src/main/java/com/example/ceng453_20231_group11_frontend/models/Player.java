05da8c58f126eb211904edb13cb8628464fed9f4
berke <berke.ahlatci@gmail.com>
2023-12-15 15:24:15 +0300

    update resources

--- src/main/java/com/example/ceng453_20231_group11_frontend/models/Player.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Player.java
@@ -1,8 +1,11 @@
 package com.example.ceng453_20231_group11_frontend.models;
 
+import com.example.ceng453_20231_group11_frontend.enums.ResourceType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashMap;
 
 @Getter
 @Setter
@@ -11,12 +14,15 @@ public class Player {
     private int score = 0;
     private boolean hasLongestRoad = false;
 
-    private int bricks = 0;
-    private int lumbers = 0;
-    private int ores = 0;
-    private int grains = 0;
-    private int wools = 0;
+    private HashMap<ResourceType, Integer> resources = new HashMap<>() {{
+        put(ResourceType.LUMBER, 0);
+        put(ResourceType.BRICK, 0);
+        put(ResourceType.GRAIN, 0);
+        put(ResourceType.WOOL, 0);
+        put(ResourceType.ORE, 0);
+    }};
 
+    // TODO: Update type to circles
     private int roads; //TODO
     private int settlements; //TODO
     private int cities; //TODO
@@ -31,7 +37,8 @@ public class Player {
 
     private boolean canBuildRoad() {
         boolean isSpaceExists = true; // TODO road available logic
-        return (lumbers > 0 && bricks > 0 && isSpaceExists);
+        return true;
+        // return (lumbers > 0 && bricks > 0 && isSpaceExists);
     }
 
     private boolean isEdgeRoadBuildable(Integer edgeId) {
@@ -40,8 +47,8 @@ public class Player {
 
     private boolean buildRoad(Integer edgeId) { // TODO true builded (skip turn) / false continue to the while loop after warning
         if (isEdgeRoadBuildable(edgeId)) {
-            lumbers--;
-            bricks--;
+            // lumbers--;
+            // bricks--;
             // TODO add road to board and roads of this class
             return true;
         } else {
@@ -52,7 +59,8 @@ public class Player {
 
     private boolean canBuildSettlement() {
         boolean isSpaceExists = true; // TODO settlement available logic
-        return (lumbers > 0 && bricks > 0 && grains > 0 && wools > 0 && isSpaceExists);
+        return true;
+        //return (lumbers > 0 && bricks > 0 && grains > 0 && wools > 0 && isSpaceExists);
     }
 
     private boolean isSettlementBuildable(Integer vertexId) {
@@ -61,10 +69,10 @@ public class Player {
 
     private boolean buildSettlement(Integer vertexId) {
         if (isSettlementBuildable(vertexId)) {
-            lumbers--;
-            bricks--;
-            grains--;
-            wools--;
+            // lumbers--;
+            // bricks--;
+            // grains--;
+            // wools--;
             score++;
             // TODO add settlement to board and settlements of this class
             return true;
@@ -76,7 +84,8 @@ public class Player {
 
     private boolean canBuildCity() {
         boolean isSettlementExists = true; // TODO
-        return (ores > 2 && grains > 1 && isSettlementExists);
+        return true;
+        // return (ores > 2 && grains > 1 && isSettlementExists);
     }
 
     private boolean isCityBuildable(Integer vertexId) {
@@ -85,8 +94,8 @@ public class Player {
 
     private boolean buildCity(Integer vertexId) {
         if (isCityBuildable(vertexId)) {
-            ores -= 3;
-            grains -= 2;
+            // ores -= 3;
+            // grains -= 2;
             score++;
             // TODO add settlement to board and settlements of this class
             return true;
