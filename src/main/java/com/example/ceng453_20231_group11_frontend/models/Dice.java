02f78c15bd24e02584a7db37bc8107d3a5ed8e8c
berke <berke.ahlatci@gmail.com>
2023-12-16 18:27:08 +0300

    refactor dice methods

--- src/main/java/com/example/ceng453_20231_group11_frontend/models/Dice.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Dice.java
@@ -1,15 +1,17 @@
 package com.example.ceng453_20231_group11_frontend.models;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Random;
 
+@Getter
 @NoArgsConstructor
 public class Dice {
-    Integer die1 = 0;
-    Integer die2 = 0;
+    private Integer die1 = 0;
+    private Integer die2 = 0;
 
-    Integer diceTotal = 0;
+    private Integer diceTotal = 0;
 
     public void roll() {
         Random random = new Random();
@@ -18,15 +20,15 @@ public class Dice {
         this.diceTotal = this.die1 + this.die2;
     }
 
-    public String getDie1() {
+    public String getStringDie1() {
         return this.die1.toString();
     }
 
-    public String getDie2() {
+    public String getStringDie2() {
         return this.die2.toString();
     }
 
-    public String getDiceTotal() {
+    public String getStringDieTotal() {
         return this.diceTotal.toString();
     }
 }
