e2af88b5ce89c1d28ce715eed1471f662cb992f4
berke <berke.ahlatci@gmail.com>
2023-12-16 18:44:21 +0300

    remove next() from turn state

--- src/main/java/com/example/ceng453_20231_group11_frontend/enums/TurnState.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/enums/TurnState.java
@@ -4,10 +4,4 @@ public enum TurnState {
     INITIALIZATION,
     ROLL_DICE,
     TURN_PLAYER;
-
-    public TurnState next() {
-        // Find the index of the next state and wrap around if it reaches the end
-        int nextIndex = (this.ordinal() + 1) % TurnState.values().length;
-        return TurnState.values()[nextIndex];
-    }
 }
