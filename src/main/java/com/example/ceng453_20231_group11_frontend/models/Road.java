7a8f38e86de3ff657c5d885b36bd9674e297a090
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-21 22:17:16 +0300

    Refactor building models

--- src/main/java/com/example/ceng453_20231_group11_frontend/models/Road.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Road.java
@@ -2,12 +2,19 @@ package com.example.ceng453_20231_group11_frontend.models;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
 
 public class Road extends Group {
-    public Road() {
-        Rectangle rectangle = new Rectangle(50, 10);
-        rectangle.setFill(Color.GRAY);
-        this.getChildren().add(rectangle);
+    // Constructor for the Road class
+    public Road(double startX, double startY, double endX, double endY) {
+        // Create a new Line object to represent the road
+        Line line = new Line(startX, startY, endX, endY);
+
+        // Set the visual properties of the line
+        line.setStroke(Color.GRAY); // Color of the road
+        line.setStrokeWidth(3); // Width of the road
+
+        // Add the line to the group
+        this.getChildren().add(line);
     }
 }
--- src/main/java/com/example/ceng453_20231_group11_frontend/models/Settlement.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Settlement.java
@@ -2,19 +2,22 @@ package com.example.ceng453_20231_group11_frontend.models;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 
 public class Settlement extends Group {
     private final Integer victoryPoint = 1;
     public Settlement() {
-        Polygon polygon = new Polygon();
-        polygon.getPoints().addAll(new Double[]{
-                0.0, 0.0,
-                10.0, 10.0,
-                0.0, 20.0,
-                -10.0, 10.0
-        });
-        polygon.setFill(Color.BLACK);
-        this.getChildren().add(polygon);
+        // Create a rectangle to represent the settlement
+        Rectangle rectangle = new Rectangle();
+
+        // Set the width and height of the rectangle
+        rectangle.setWidth(20.0);
+        rectangle.setHeight(20.0);
+
+        // Set the fill color for the rectangle
+        rectangle.setFill(Color.BLACK);
+
+        // Add the rectangle to the group
+        this.getChildren().add(rectangle);
     }
 }
