c362c7c6250e000ae5b1a8c86e8a641af292d2f2
berke <berke.ahlatci@gmail.com>
2023-12-22 15:13:12 +0300

    update PlayerColor

--- src/main/java/com/example/ceng453_20231_group11_frontend/enums/PlayerColor.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/enums/PlayerColor.java
@@ -1,8 +1,18 @@
 package com.example.ceng453_20231_group11_frontend.enums;
 
+import javafx.scene.paint.Color;
+import lombok.Getter;
+
+@Getter
 public enum PlayerColor {
-    RED,
-    BLUE,
-    GREEN,
-    ORANGE
+    RED(Color.RED),
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN),
+    ORANGE(Color.ORANGE);  // Define more colors if needed
+
+    private final Color color;
+
+    PlayerColor(Color color) {
+        this.color = color;
+    }
 }
