0594e396fee0ad8e0722391b57224f36d82d1367
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-12-22 16:37:29 +0300

    Update settlement model

--- src/main/java/com/example/ceng453_20231_group11_frontend/models/Settlement.java
+++ src/main/java/com/example/ceng453_20231_group11_frontend/models/Settlement.java
@@ -2,22 +2,14 @@ package com.example.ceng453_20231_group11_frontend.models;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
-public class Settlement extends Group {
+public class Settlement extends Rectangle {
     private final Integer victoryPoint = 1;
-    public Settlement() {
-        // Create a rectangle to represent the settlement
-        Rectangle rectangle = new Rectangle();
-
-        // Set the width and height of the rectangle
-        rectangle.setWidth(20.0);
-        rectangle.setHeight(20.0);
-
-        // Set the fill color for the rectangle
-        rectangle.setFill(Color.BLACK);
-
-        // Add the rectangle to the group
-        this.getChildren().add(rectangle);
+    public Settlement(Circle circle, Color color, Group board) {
+        super(circle.getLayoutX() - 10, circle.getLayoutY() - 10, 20, 20);
+        this.setFill(color);
+        board.getChildren().add(this);
     }
 }
